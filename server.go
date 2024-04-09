b739b1bf30e193ba14441fc391ff46df493a5abe
DanielSalazar88 <daniel_salazar82201@elpoli.edu.co>
2024-04-09 09:28:52 -0500

    REFACTOR CODE

--- server.go
+++ server.go
@@ -1,28 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"pet-s.a-graphql/graph"
+	"pet-s.a-graphql/settings"
 )
 
-const defaultPort = "8080"
+var s = settings.ReadYaml()
+
+var defaultPort = fmt.Sprintf("%v", s.Handlers.Port)
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", defaultPort)
+	log.Fatal(http.ListenAndServe(":"+defaultPort, nil))
 }
